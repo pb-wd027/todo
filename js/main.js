205ff61a9fb315dbda2d2582e4b877afe1282f8d
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-05-23 14:05:43 +0200

    completed and delete functionality added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,55 @@
-function addItem(){
+const addbtn=document.getElementById("btn-primary-add");
+const tasklistContent = document.getElementById('task-list-content');
+const todoItemsList = document.getElementById('todo-item-lists');
 
-    //alert('please add item');
-    let item=document.getElementById('itemInput').value; //input value
-    let textItem=document.createTextNode(item);//
+
+
+function addItem(){
+   
+    let itemInputData=document.getElementById('itemInput').value; //input value
+    let textItem=document.createTextNode(itemInputData);
     let newItem=document.createElement("li");
-     newItem.appendChild(textItem);
-     document.getElementById("todoListitem").appendChild(newItem);
+    newItem.classList.add('container');
+    newItem.style.boxshadow="1px 1px 5px 1px gray";
+
+    //delete functionality
+    const deleteButton=document.createElement('button');
+    deleteButton.innerText='DEL';   
+    deleteButton.addEventListener('click', () => {
+        alert('Are you sure ? you want to delete the task?')
+    todoItemsList.removeChild(newItem);
+    todoItemsList.removeChild(deleteButton);
+    todoItemsList.removeChild(completeButton);
+    todoItemsList.removeChild(editButton);
+    });
+
+    //complete task functionality
+    const completeButton=document.createElement('button');
+    completeButton.innerText='Complete';    
+    completeButton.addEventListener('click', () => {
+        newItem.innerHTML= itemInputData.strike();
+    });
+
+    // edit button Functionality
+    const editButton=document.createElement('button');
+    editButton.innerText='Edit';    
+    editButton.addEventListener('click', () => {
+        todoItemsList.classList.add('done');
+    });
+    //Add value in a list
+    console.log("Entered value in list is--"+itemInputData);
+    if (itemInputData ===''){
+        alert('please add item ...');
+        return;
+    }else{
+        newItem.appendChild(textItem);
+        newItem.appendChild(completeButton);
+        newItem.appendChild(editButton);
+        newItem.appendChild(deleteButton);
+        document.getElementById("todo-item-lists").appendChild(newItem);    
+    
+    }
 
 }
-document.getElementById("btn-primary-add").addEventListener("click", addItem);
+
+addbtn.addEventListener("click", addItem);
